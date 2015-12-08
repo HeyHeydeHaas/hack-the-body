62e726a3fa0737f81fde87bbc94707e7164108f7
Accommodavid <davidvgdn@me.com>
2015-12-08 11:20:44 +0100

    Add random segment width

--- src/app.js
+++ src/app.js
@@ -1,16 +1,35 @@
-/* global app */
+/* global canvas, app */
 import React from 'react'
 import ReactDOM from 'react-dom'
 import paper from 'paper/dist/paper-full'
 
 class App extends React.Component {
+  regenerateSegments () {
+    let logo = window.paper.project.layers[0] ? window.paper.project.layers[0].children['logo'] : false
+    if (logo) {
+      logo.children.map((segment) => {
+        segment.strokeWidth = 40 * Math.random()
+      })
+    }
+    window.paper.view.update()
+  }
+
   componentWillMount () {
-    paper.setup(app)
-    paper.project.importSVG('./assets/htb.svg')
+    window.paper = paper.setup(canvas)
+    paper.project.importSVG('./assets/htb.svg', (logo) => {
+      // Place the logo 50px from the corner
+      logo.name = 'logo'
+      logo.pivot = logo.bounds.topLeft
+      logo.position = new paper.Point(50, 50)
+      // Do the random generation
+      this.regenerateSegments()
+    })
   }
 
   render () {
-    return false
+    return (
+      <button onClick={this.regenerateSegments}>Regenerate</button>
+    )
   }
 }
 
